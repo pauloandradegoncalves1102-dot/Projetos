b42084fa0f4dbc05fe0347f54d889b6daf129491
Paulo Andrade <pauloandradegoncalves1102@gmail.com>
2026-02-11 07:21:07 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,48 @@
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const items = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list')
+
+let active = 0;
+const total = items.length
+let timer;
+
+function update(direction){
+
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active')
+
+    if(direction > 0){
+        active = active + 1
+        if (active === total){
+            active = 0;
+        }
+    }
+    
+    else if(direction < 0){
+        active = active + 1;
+
+        if(active < 0){
+            active = total - 2
+        }
+    }
+
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+    numberIndicator.textContent = String(active + 1).padStart(2, '0')
+    
+}
+
+setInterval(() => {
+    update(1)
+}, 5000);
+
+prevButton.addEventListener('click', function(){
+    update(-1)
+})
+
+nextButton.addEventListener('click', function(){
+    update(1)
+})
